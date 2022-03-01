d15197fa69095d1ba79506de690045b5f0f65ec9
hananmohammed <hananmohammed2468@gmail.com>
2022-03-01 10:32:47 +0200

    Adding the Visual Components of the Realtime Game

--- resources/views/game/show.blade.php
+++ resources/views/game/show.blade.php
@@ -1,7 +1,17 @@
 @extends('layouts.app')
 @push('styles')
-    <style>
-
+    <style type="text/css">
+        @keyframes rotate{
+            from{
+                transform: rotate(0deg);
+            }
+            to{
+                transform: rotate(360deg);
+            }
+        }
+        .refresh{
+            animation: rotate 1.5s linear infinite;
+        }
     </style>
 @endpush
 @section('content')
@@ -12,7 +22,23 @@
                     <div class="card-header">Game</div>
                     <div class="card-body">
                         <div class="text-center">
-                            <img src="{{asset('img/circle.png')}}" alt="">
+                            <img class="refresh" src="{{ asset('assets/images/circle.png') }}" height="250" width="250"  alt="circle" >
+                            <p id="winner" class="display-1 d-none text-primary" ></p>
+                        </div>
+                        <hr />
+                        <div class="text-center">
+                            <label class="font-weight-bold h5"> Your Bet </label>
+                            <select class="custom-select col-auto">
+                                <option selected> Not in  </option>
+                                @foreach ( range(1, 12) as $number )
+                                <option>{{ $number }}</option>
+                                @endforeach
+                            </select>
+                            <hr/>
+                            <p class="font-weight-bold h5" > Remaining Time </p>
+                            <p id="timer" class="h5 text-danger">Waiting To Start </p>
+                            <hr/>
+                            <p id="result" class="h1"></p>
                         </div>
                     </div>
                 </div>
